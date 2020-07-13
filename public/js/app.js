e8e234eb9e437276d5c21e02f693deb633c142ec
AhmadKheder <ahmadkhederabdulatef@gmail.com>
2020-07-14 01:22:15 +0300

    init components

--- public/js/app.js
+++ public/js/app.js
@@ -15,3 +15,6 @@ function burger(){
     links.style.display = 'none';
     quit.style.display = 'none';
   }
+  $('#viewButton').on('click', function () {
+    $('.toggleForm').slideToggle();
+  });
