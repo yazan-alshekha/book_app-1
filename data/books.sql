4227b228aa61ddc1e19e9f77555c722c3faedf5f
yazan <yazan.alshekha@outlook.com>
2020-07-17 23:14:43 +0300

    lab11

--- data/books.sql
+++ data/books.sql
@@ -1,6 +1,5 @@
 DROP TABLE IF EXISTS books;
-CREATE TABLE
-IF NOT EXISTS books
+CREATE TABLE IF NOT EXISTS books
 (
     id SERIAL PRIMARY KEY,
     author VARCHAR(255),
@@ -14,4 +13,3 @@ IF NOT EXISTS books
 
 INSERT INTO books (author,title,image_url,description) VALUES('AHMAD','from the future', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSo7qnvwvxl5KihCobp4tqiIshkaZj8YrRAeA&usqp=CAU',  'THIS IS A DRAFT');
 INSERT INTO books (author,title,image_url,description) VALUES('AHMAD','from the future', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSo7qnvwvxl5KihCobp4tqiIshkaZj8YrRAeA&usqp=CAU',  'THIS IS A DRAFT');
--- INSERT INTO books (title,image_url,author,description) VALUES('from the future', 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSo7qnvwvxl5KihCobp4tqiIshkaZj8YrRAeA&usqp=CAU', 'AHMAD', 'THIS IS A DRAFT');
